d85e1ab6c0a663ce3dfff469f2971681daae9ff3
Gerhard Waldemair <gerhard@modified-shop.org>
2024-10-16 10:14:16 +0200

    add new method and extend routes

--- includes/external/api/v1/Action/ProductAction.php
+++ includes/external/api/v1/Action/ProductAction.php
@@ -94,26 +94,16 @@
               $tags[] = $products_tags;
             }
 
-            $xsell = array();
-            $products_xsell_query = xtc_db_query("SELECT *
-                                                    FROM ".TABLE_PRODUCTS_XSELL."
-                                                   WHERE products_id = '".(int)$productId."'
-                                                ORDER BY sort_order, ID");
-            while ($products_xsell = xtc_db_fetch_array($products_xsell_query)) {
-              $xsell[] = $products_xsell;
-            }
-          }
-          
           $result = [
             'products' => $this->GetProduct($productId, false),
             'products_description' => $this->GetProductDescription($productId, false),
             'products_to_categories' => $this->GetProductCategories($productId, false),
             'products_images' => $this->GetProductImages($productId, false),
+            'products_xsell' => $this->GetProductXsell($productId, false),
             'products_attributes' => $attributes,
             'products_tags' => $tags,
-            'products_xsell' => $xsell,
           ];
-          
+
           $result = $this->encode_request($result);
           return $result;
       }
@@ -486,6 +476,42 @@
           }
 
           $result = $this->encode_request($images);
+          return $result;
+      }
+
+      /**
+       * Read a Product xsel by the given Product id.
+       *
+       * @param int $productId The Product id
+       *
+       * @throws Exception
+       *
+       * @return array The Product data
+       */
+      public function GetProductXsell(int $productId, $Exception = true): array
+      {
+          // Input validation
+          if (empty($productId)) {
+              throw new Exception('Product ID required');
+          }
+
+          $product_query = xtc_db_query("SELECT *
+                                           FROM ".TABLE_PRODUCTS_XSELL."
+                                          WHERE products_id = '".(int)$productId."'");
+          if (xtc_db_num_rows($product_query) < 1 && $Exception === true) {
+              throw new Exception(sprintf('Product xsell not found: %s', $productId));
+          } else {
+              $xsell = array();
+              $products_xsell_query = xtc_db_query("SELECT *
+                                                      FROM ".TABLE_PRODUCTS_XSELL."
+                                                     WHERE products_id = '".(int)$productId."'
+                                                  ORDER BY sort_order, ID");
+              while ($products_xsell = xtc_db_fetch_array($products_xsell_query)) {
+                  $xsell[] = $products_xsell;
+              }
+          }
+
+          $result = $this->encode_request($xsell);
           return $result;
       }
 
