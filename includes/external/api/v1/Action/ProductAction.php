11145ca87c2f77cb92049a00c6e811d5c0e29c96
Gerhard Waldemair <gerhard@modified-shop.org>
2024-10-16 10:45:27 +0200

    add routine get attributes

--- includes/external/api/v1/Action/ProductAction.php
+++ includes/external/api/v1/Action/ProductAction.php
@@ -75,16 +75,7 @@
           if (xtc_db_num_rows($product_query) < 1) {
               throw new Exception(sprintf('Product not found: %s', $productId));
           } else {
-  
-              $attributes = array();
-              $products_attributes_query = xtc_db_query("SELECT *
-                                                           FROM ".TABLE_PRODUCTS_ATTRIBUTES."
-                                                          WHERE products_id = '".(int)$productId."'
-                                                       ORDER BY sortorder, products_attributes_id");
-              while ($products_attributes = xtc_db_fetch_array($products_attributes_query)) {
-                $attributes[] = $products_attributes;
-              }
-  
+
               $tags = array();
               $products_tags_query = xtc_db_query("SELECT *
                                                      FROM ".TABLE_PRODUCTS_TAGS."
@@ -93,17 +84,17 @@
               while ($products_tags = xtc_db_fetch_array($products_tags_query)) {
                 $tags[] = $products_tags;
               }
-  
+
               $result = [
                 'products' => $this->GetProduct($productId, false),
                 'products_description' => $this->GetProductDescription($productId, false),
                 'products_to_categories' => $this->GetProductCategories($productId, false),
                 'products_images' => $this->GetProductImages($productId, false),
                 'products_xsell' => $this->GetProductXsell($productId, false),
-                'products_attributes' => $attributes,
+                'products_attributes' => $this->GetProductAttributes($productId, false),
                 'products_tags' => $tags,
               ];
-    
+
               $result = $this->encode_request($result);
               return $result;
           }
@@ -480,7 +471,7 @@
       }
 
       /**
-       * Read a Product xsel by the given Product id.
+       * Read a Product xsell by the given Product id.
        *
        * @param int $productId The Product id
        *
@@ -512,6 +503,42 @@
           }
 
           $result = $this->encode_request($xsell);
+          return $result;
+      }
+
+      /**
+       * Read a Product attributes by the given Product id.
+       *
+       * @param int $productId The Product id
+       *
+       * @throws Exception
+       *
+       * @return array The Product data
+       */
+      public function GetProductAttributes(int $productId, $Exception = true): array
+      {
+          // Input validation
+          if (empty($productId)) {
+              throw new Exception('Product ID required');
+          }
+
+          $product_query = xtc_db_query("SELECT *
+                                           FROM ".TABLE_PRODUCTS_ATTRIBUTES."
+                                          WHERE products_id = '".(int)$productId."'");
+          if (xtc_db_num_rows($product_query) < 1 && $Exception === true) {
+              throw new Exception(sprintf('Product attributes not found: %s', $productId));
+          } else {
+              $attributes = [];
+              $products_attributes_query = xtc_db_query("SELECT *
+                                                           FROM ".TABLE_PRODUCTS_ATTRIBUTES."
+                                                          WHERE products_id = '".(int)$productId."'
+                                                       ORDER BY sortorder, products_attributes_id");
+              while ($products_attributes = xtc_db_fetch_array($products_attributes_query)) {
+                  $attributes[] = $products_attributes;
+              }
+          }
+
+          $result = $this->encode_request($attributes);
           return $result;
       }
 
