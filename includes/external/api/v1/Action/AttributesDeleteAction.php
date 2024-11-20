fc7e489ab2bd0a702be7d4554ceffe0c3e00b7fb
Gerhard Waldemair <gerhard@modified-shop.org>
2024-11-20 13:27:17 +0100

    add attributes route

--- includes/external/api/v1/Action/AttributesDeleteAction.php
+++ includes/external/api/v1/Action/AttributesDeleteAction.php
@@ -22,6 +22,66 @@
   trait AttributesDeleteAction
   {
       /**
+       * Delete a option by the given option id.
+       *
+       * @param int $optionId The option id
+       *
+       * @throws Exception
+       *
+       * @return void
+       */
+      public function DeleteOption(int $optionId): void
+      {
+          // Input validation
+          if (empty($optionId)) {
+              throw new Exception('Option ID required');
+          }
+
+          $option_query = xtc_db_query("SELECT *
+                                          FROM ".TABLE_PRODUCTS_OPTIONS."
+                                         WHERE products_options_id = '".(int)$optionId."'");
+          if (xtc_db_num_rows($option_query) < 1 && $this->throw_exception === true) {
+              throw new Exception(sprintf('Option not found: %s', $optionId));
+          } else {
+              xtc_db_query("DELETE FROM ".TABLE_PRODUCTS_OPTIONS." 
+                                  WHERE products_options_id = '".(int)$optionId."'");
+
+              xtc_db_query("DELETE FROM ".TABLE_PRODUCTS_OPTIONS_VALUES_TO_PRODUCTS_OPTIONS." 
+                                  WHERE products_options_id = '".(int)$optionId."'");
+          }
+      }
+
+      /**
+       * Delete a value by the given value id.
+       *
+       * @param int $valueId The value id
+       *
+       * @throws Exception
+       *
+       * @return void
+       */
+      public function DeleteValue(int $valueId): void
+      {
+          // Input validation
+          if (empty($valueId)) {
+              throw new Exception('Value ID required');
+          }
+
+          $value_query = xtc_db_query("SELECT *
+                                         FROM ".TABLE_PRODUCTS_OPTIONS_VALUES."
+                                        WHERE products_options_values_id = '".(int)$valueId."'");
+          if (xtc_db_num_rows($value_query) < 1 && $this->throw_exception === true) {
+              throw new Exception(sprintf('Option value not found: %s', $valueId));
+          } else {
+              xtc_db_query("DELETE FROM ".TABLE_PRODUCTS_OPTIONS_VALUES." 
+                                  WHERE products_options_values_id = '".(int)$valueId."'");
+
+              xtc_db_query("DELETE FROM ".TABLE_PRODUCTS_OPTIONS_VALUES_TO_PRODUCTS_OPTIONS." 
+                                  WHERE products_options_values_id = '".(int)$valueId."'");
+          }
+      }
+
+      /**
        * Delete a value from options by the given option id and value id.
        *
        * @param int $optionId The option id
@@ -43,17 +103,17 @@
               $where = "AND products_options_values_id = '".(int)$valueId."'";
           }
 
-          $product_query = xtc_db_query("SELECT *
-                                           FROM ".TABLE_PRODUCTS_OPTIONS_VALUES_TO_PRODUCTS_OPTIONS."
-                                          WHERE products_options_id = '".(int)$optionId."'
-                                                ".$where);
-          if (xtc_db_num_rows($product_query) < 1 && $this->throw_exception === true) {
+          $attributes_query = xtc_db_query("SELECT *
+                                              FROM ".TABLE_PRODUCTS_OPTIONS_VALUES_TO_PRODUCTS_OPTIONS."
+                                             WHERE products_options_id = '".(int)$optionId."'
+                                                   ".$where);
+          if (xtc_db_num_rows($attributes_query) < 1 && $this->throw_exception === true) {
               throw new Exception(sprintf('Option value not found: %s', $optionId));
           } else {
-              while ($product = xtc_db_fetch_array($product_query)) {
+              while ($attributes = xtc_db_fetch_array($attributes_query)) {
                   xtc_db_query("DELETE FROM ".TABLE_PRODUCTS_OPTIONS_VALUES_TO_PRODUCTS_OPTIONS." 
                                       WHERE products_options_id = '".(int)$optionId."'
-                                        AND products_options_values_id = '".(int)$product['products_options_values_id']."'");
+                                        AND products_options_values_id = '".(int)$attributes['products_options_values_id']."'");
               }
           }
       }
@@ -61,7 +121,7 @@
       /**
        * Delete all values from options by the given option id.
        *
-       * @param int $optionId The product id
+       * @param int $optionId The option id
        *
        * @throws Exception
        *
--- includes/external/api/v1/config/attributes.php
+++ includes/external/api/v1/config/attributes.php
@@ -14,13 +14,19 @@
   $app->get('/attributes/options',                    \api\v1\Service\Attributes\GetOptions::class);
   $app->get('/attributes/options/{id}',               \api\v1\Service\Attributes\GetSingleOption::class);
 
+  // delete attributes
+  $app->delete('/attributes/options/{id}',            \api\v1\Service\Attributes\DeleteOption::class);
+
   // values
   $app->get('/attributes/values',                     \api\v1\Service\Attributes\GetValues::class);
   $app->get('/attributes/values/{id}',                \api\v1\Service\Attributes\GetSingleValue::class);
 
+  // delete attributes
+  $app->delete('/attributes/values/{id}',             \api\v1\Service\Attributes\DeleteValue::class);
+
   // attributes
   $app->get('/attributes/{id}',                       \api\v1\Service\Attributes\GetAttributes::class);
 
-  // delete products
+  // delete attributes
   $app->delete('/attributes/{id}/values',             \api\v1\Service\Attributes\DeleteAllAttributes::class);
   $app->delete('/attributes/{id}/values/{vid}',       \api\v1\Service\Attributes\DeleteAttributes::class);
