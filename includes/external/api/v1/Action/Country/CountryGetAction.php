9f31cc20bf593c2358a9f6f80921b2a9c2fdbaec
GTB <gerhard@modified-shop.org>
2025-03-14 17:33:13 +0100

    add geo zone route

--- includes/external/api/v1/Action/Country/CountryGetAction.php
+++ includes/external/api/v1/Action/Country/CountryGetAction.php
@@ -121,5 +121,87 @@
           
           return $result;
       }
+
+      /**
+       * Read a geo zone by the given geo zone id.
+       *
+       * @param int $geoZoneId The category id
+       *
+       * @throws Exception
+       *
+       * @return array The geo zone data
+       */
+      public function GetSingleGeoZone(int $geoZoneId): array
+      {
+          // Input validation
+          if (empty($geoZoneId)) {
+              throw new Exception('Country ID required');
+          }
+          
+          $geo_zone_query = xtc_db_query("SELECT *
+                                            FROM ".TABLE_GEO_ZONES."
+                                           WHERE geo_zone_id = '".(int)$geoZoneId."'");
+          if (xtc_db_num_rows($geo_zone_query) < 1) {
+              throw new Exception(sprintf('Geo Zone not found: %s', $geoZoneId));
+          } else {
+              $geo_zone = xtc_db_fetch_array($geo_zone_query);
+          }
+          
+          $result = $this->encode_request($geo_zone);
+          return $result;
+      }
+
+      /**
+       * Read geo zone by given conditions
+       *
+       * @param mixed[] $options
+       *
+       * @throws Exception
+       *
+       * @return array The geo zone data
+       */
+      public function GetGeoZones(array $options): array
+      {          
+          /* Store passed in options overwriting any defaults */
+          $this->hydrate($options);
+          
+          if ($this->options['limit'] > 50) $this->options['limit'] = 50;
+          $this->options['page'] = (abs((int)$this->options['page']) > 0) ? abs((int)$this->options['page']) : 1;
+                                                        
+          $count_query = xtc_db_query("SELECT count(*) as total
+                                         FROM ".TABLE_GEO_ZONES);
+          $count = xtc_db_fetch_array($count_query);
+          
+          if ($count['total'] < 1) {
+              throw new Exception('no Country found');
+          }
+          
+          $data = [];
+          $geo_zone_query = xtc_db_query("SELECT geo_zone_id
+                                             FROM ".TABLE_GEO_ZONES."
+                                         ORDER BY geo_zone_id ASC
+                                            LIMIT ".(($this->options['page'] - 1) * $this->options['limit']).", ".$this->options['limit']);
+          while ($geo_zone = xtc_db_fetch_array($geo_zone_query)) {
+              $data[] = $this->GetSingleGeoZone($geo_zone['geo_zone_id']);
+          }
+          
+          $result = [
+              'paging' => [
+                  'total' => $count['total']
+              ],
+              'data' => $data
+          ];
+          
+          if ($count['total'] > count($data)) {
+              if ($this->options['page'] > 1) {
+                  $result['paging']['prev'] = HTTPS_SERVER.DIR_WS_CATALOG.ltrim($this->options['path'], '/').'?'.xtc_get_all_get_params(array('page')).'page='.($this->options['page'] - 1);
+              }
+              if (((($this->options['page'] - 1) * $this->options['limit']) + $this->options['limit']) < $count['total']) {
+                  $result['paging']['next'] = HTTPS_SERVER.DIR_WS_CATALOG.ltrim($this->options['path'], '/').'?'.xtc_get_all_get_params(array('page')).'page='.($this->options['page'] + 1);
+              }
+          }
+          
+          return $result;
+      }
     
   }
