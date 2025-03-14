9543e33c5955f173a1ded44bf66db601184668ce
GTB <gerhard@modified-shop.org>
2025-03-14 17:04:55 +0100

    fix route for countries

--- includes/external/api/v1/config/countries.php
+++ includes/external/api/v1/config/countries.php
@@ -11,20 +11,20 @@
    ---------------------------------------------------------------------------------------*/
 
   // customers
-  $app->get('/countries',                           \api\v1\Service\Customer\GetCountries::class);
-  $app->get('/countries/{id}',                      \api\v1\Service\Customer\GetSingleCountry::class);
+  $app->get('/countries',                           \api\v1\Service\Country\GetCountries::class);
+  $app->get('/countries/{id}',                      \api\v1\Service\Country\GetSingleCountry::class);
   
-  $app->get('/countries/geo_zones',                 \api\v1\Service\Customer\GetGeoZones::class);
-  $app->get('/countries/geo_zones/{id}',            \api\v1\Service\Customer\GetSingleGeoZone::class);
+  $app->get('/countries/geo_zones',                 \api\v1\Service\Country\GetGeoZones::class);
+  $app->get('/countries/geo_zones/{id}',            \api\v1\Service\Country\GetSingleGeoZone::class);
 
-  $app->get('/countries/tax_class',                 \api\v1\Service\Customer\GetTaxClass::class);
-  $app->get('/countries/tax_class/{id}',            \api\v1\Service\Customer\GetSingleTaxClass::class);
+  $app->get('/countries/tax_class',                 \api\v1\Service\Country\GetTaxClass::class);
+  $app->get('/countries/tax_class/{id}',            \api\v1\Service\Country\GetSingleTaxClass::class);
 
-  $app->get('/countries/tax_rates',                 \api\v1\Service\Customer\GetTaxRates::class);
-  $app->get('/countries/tax_rates/{id}',            \api\v1\Service\Customer\GetSingleTaxRate::class);
+  $app->get('/countries/tax_rates',                 \api\v1\Service\Country\GetTaxRates::class);
+  $app->get('/countries/tax_rates/{id}',            \api\v1\Service\Country\GetSingleTaxRate::class);
 
-  $app->get('/countries/zones',                     \api\v1\Service\Customer\GetZones::class);
-  $app->get('/countries/zones/{id}',                \api\v1\Service\Customer\GetSingleZone::class);
+  $app->get('/countries/zones',                     \api\v1\Service\Country\GetZones::class);
+  $app->get('/countries/zones/{id}',                \api\v1\Service\Country\GetSingleZone::class);
 
-  $app->get('/countries/zones_to_geo_zones',        \api\v1\Service\Customer\GetZonesToGeoZones::class);
-  $app->get('/countries/zones_to_geo_zones/{id}',   \api\v1\Service\Customer\GetSingleZonesToGeoZone::class);
+  $app->get('/countries/zones_to_geo_zones',        \api\v1\Service\Country\GetZonesToGeoZones::class);
+  $app->get('/countries/zones_to_geo_zones/{id}',   \api\v1\Service\Country\GetSingleZonesToGeoZone::class);
