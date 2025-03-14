46a43b8e641667fe50a7568dd96312c75dfb10b4
GTB <gerhard@modified-shop.org>
2025-03-14 17:54:00 +0100

    add tax class route

--- includes/external/api/v1/Action/Country/CountryGetAction.php
+++ includes/external/api/v1/Action/Country/CountryGetAction.php
@@ -135,7 +135,7 @@
       {
           // Input validation
           if (empty($geoZoneId)) {
-              throw new Exception('Country ID required');
+              throw new Exception('Geo Zone ID required');
           }
           
           $geo_zone_query = xtc_db_query("SELECT *
@@ -175,16 +175,100 @@
           $count = xtc_db_fetch_array($count_query);
           
           if ($count['total'] < 1) {
-              throw new Exception('no Country found');
+              throw new Exception('no Geo Zone found');
           }
           
           $data = [];
           $geo_zone_query = xtc_db_query("SELECT geo_zone_id
-                                             FROM ".TABLE_GEO_ZONES."
-                                         ORDER BY geo_zone_id ASC
-                                            LIMIT ".(($this->options['page'] - 1) * $this->options['limit']).", ".$this->options['limit']);
+                                            FROM ".TABLE_GEO_ZONES."
+                                        ORDER BY geo_zone_id ASC
+                                           LIMIT ".(($this->options['page'] - 1) * $this->options['limit']).", ".$this->options['limit']);
           while ($geo_zone = xtc_db_fetch_array($geo_zone_query)) {
               $data[] = $this->GetSingleGeoZone($geo_zone['geo_zone_id']);
+          }
+          
+          $result = [
+              'paging' => [
+                  'total' => $count['total']
+              ],
+              'data' => $data
+          ];
+          
+          if ($count['total'] > count($data)) {
+              if ($this->options['page'] > 1) {
+                  $result['paging']['prev'] = HTTPS_SERVER.DIR_WS_CATALOG.ltrim($this->options['path'], '/').'?'.xtc_get_all_get_params(array('page')).'page='.($this->options['page'] - 1);
+              }
+              if (((($this->options['page'] - 1) * $this->options['limit']) + $this->options['limit']) < $count['total']) {
+                  $result['paging']['next'] = HTTPS_SERVER.DIR_WS_CATALOG.ltrim($this->options['path'], '/').'?'.xtc_get_all_get_params(array('page')).'page='.($this->options['page'] + 1);
+              }
+          }
+          
+          return $result;
+      }
+
+      /**
+       * Read a tax class by the given tax class id.
+       *
+       * @param int $taxClassId The category id
+       *
+       * @throws Exception
+       *
+       * @return array The tax class data
+       */
+      public function GetSingleTaxClass(int $taxClassId): array
+      {
+          // Input validation
+          if (empty($taxClassId)) {
+              throw new Exception('Tax Class ID required');
+          }
+          
+          $tax_class_query = xtc_db_query("SELECT *
+                                             FROM ".TABLE_TAX_CLASS."
+                                            WHERE tax_class_id = '".(int)$geoZoneId."'");
+          if (xtc_db_num_rows($tax_class_query) < 1) {
+              throw new Exception(sprintf('Geo Zone not found: %s', $geoZoneId));
+          } else {
+              $tax_class = xtc_db_fetch_array($tax_class_query);
+              $tax_class['tax_class_title'] = $this->parse_multi_language_value($tax_class['tax_class_title']);
+              $tax_class['tax_class_description'] = $this->parse_multi_language_value($tax_class['tax_class_description']);
+          }
+          
+          $result = $this->encode_request($tax_class);
+          return $result;
+      }
+
+      /**
+       * Read geo zone by given conditions
+       *
+       * @param mixed[] $options
+       *
+       * @throws Exception
+       *
+       * @return array The tax class data
+       */
+      public function GetTaxClass(array $options): array
+      {          
+          /* Store passed in options overwriting any defaults */
+          $this->hydrate($options);
+          
+          if ($this->options['limit'] > 50) $this->options['limit'] = 50;
+          $this->options['page'] = (abs((int)$this->options['page']) > 0) ? abs((int)$this->options['page']) : 1;
+                                                        
+          $count_query = xtc_db_query("SELECT count(*) as total
+                                         FROM ".TABLE_TAX_CLASS);
+          $count = xtc_db_fetch_array($count_query);
+          
+          if ($count['total'] < 1) {
+              throw new Exception('no Tax Class found');
+          }
+          
+          $data = [];
+          $tax_class_query = xtc_db_query("SELECT tax_class_id
+                                             FROM ".TABLE_TAX_CLASS."
+                                         ORDER BY tax_class_id ASC
+                                            LIMIT ".(($this->options['page'] - 1) * $this->options['limit']).", ".$this->options['limit']);
+          while ($tax_class = xtc_db_fetch_array($tax_class_query)) {
+              $data[] = $this->GetSingleTaxClass($tax_class['tax_class_id']);
           }
           
           $result = [
