fb3f30e739a1465a3d275426b36114ec407d1e0a
GTB <gerhard@modified-shop.org>
2025-03-15 19:06:42 +0100

    insert and update currency

--- includes/external/api/v1/config/currencies.php
+++ includes/external/api/v1/config/currencies.php
@@ -16,7 +16,7 @@
 
   // insert currencies
   $app->post('/currencies',                      \api\v1\Service\Currency\InsertCurrency::class);
-  $app->put('/currencies/{id}',                  \api\v1\Service\Currency\InsertUpdateCurrency::class);
+  $app->post('/currencies/{id}',                  \api\v1\Service\Currency\InsertUpdateCurrency::class);
 
   // update currencies
   $app->put('/currencies/{id}',                  \api\v1\Service\Currency\InsertUpdateCurrency::class);
