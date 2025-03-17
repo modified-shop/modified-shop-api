83c1ed7848a8154314b2290796bb8e3cadc09a57
GTB <gerhard@modified-shop.org>
2025-03-17 11:07:07 +0100

    fix route

--- includes/external/api/v1/config/contents.php
+++ includes/external/api/v1/config/contents.php
@@ -11,18 +11,19 @@
    ---------------------------------------------------------------------------------------*/
 
   // contents
-  $app->get('/contents',                       \api\v1\Service\Content\GetContents::class);
-  $app->get('/contents/{id}',                  \api\v1\Service\Content\GetSingleContent::class);
-  $app->get('/contents/{id}/content',          \api\v1\Service\Content\GetContentContent::class);
+  $app->get('/contents',                          \api\v1\Service\Content\GetContents::class);
+  $app->get('/contents/{id}',                     \api\v1\Service\Content\GetSingleContent::class);
+  $app->get('/contents/{id}/content',             \api\v1\Service\Content\GetContentContent::class);
 
   // insert contents
-  $app->post('/contents',                      \api\v1\Service\Content\InsertContent::class);
-  $app->post('/contents/{id}',                 \api\v1\Service\Content\InsertUpdateContent::class);
-  $app->post('/contents/{id}/content',         \api\v1\Service\Content\InsertUpdateContentContent::class);
+  $app->post('/contents',                         \api\v1\Service\Content\InsertContent::class);
+  $app->post('/contents/{id}',                    \api\v1\Service\Content\InsertUpdateContent::class);
+  $app->post('/contents/{id}/content',            \api\v1\Service\Content\InsertUpdateContentContent::class);
 
   // update contents
-  $app->put('/contents/{id}',                  \api\v1\Service\Content\InsertUpdateContent::class);
+  $app->put('/contents/{id}',                     \api\v1\Service\Content\InsertUpdateContent::class);
 
   // delete contents
-  $app->delete('/contents/{id}',               \api\v1\Service\Content\DeleteContent::class);
-  $app->delete('/contents/{id}/content',       \api\v1\Service\Content\DeleteAllContentContent::class);
+  $app->delete('/contents/{id}',                  \api\v1\Service\Content\DeleteContent::class);
+  $app->delete('/contents/{id}/content',          \api\v1\Service\Content\DeleteAllContentContent::class);
+  $app->delete('/contents/{id}/content/{cid}',    \api\v1\Service\Content\DeleteContentContent::class);
