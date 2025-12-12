1391b05e8b0985eebab7b502eef9d3ec81ec5390
Gerhard Waldemair <gerhard@modified-shop.org>
2025-12-12 17:38:25 +0100

    update PHP DI

--- includes/external/DI/Definition/Resolver/ArrayResolver.php
+++ includes/external/DI/Definition/Resolver/ArrayResolver.php
@@ -12,25 +12,24 @@ use Exception;
 /**
  * Resolves an array definition to a value.
  *
+ * @template-implements DefinitionResolver<ArrayDefinition>
+ *
  * @since 5.0
  * @author Matthieu Napoli <matthieu@mnapoli.fr>
  */
 class ArrayResolver implements DefinitionResolver
 {
     /**
-     * @var DefinitionResolver
-     */
-    private $definitionResolver;
-
-    /**
      * @param DefinitionResolver $definitionResolver Used to resolve nested definitions.
      */
-    public function __construct(DefinitionResolver $definitionResolver)
-    {
-        $this->definitionResolver = $definitionResolver;
+    public function __construct(
+        private DefinitionResolver $definitionResolver,
+    ) {
     }
 
     /**
+     * {@inheritDoc}
+     *
      * Resolve an array definition to a value.
      *
      * An array definition can contain simple values or references to other entries.
@@ -42,7 +41,7 @@ class ArrayResolver implements DefinitionResolver
         $values = $definition->getValues();
 
         // Resolve nested definitions
-        array_walk_recursive($values, function (&$value, $key) use ($definition) {
+        array_walk_recursive($values, function (& $value, $key) use ($definition) {
             if ($value instanceof Definition) {
                 $value = $this->resolveDefinition($value, $definition, $key);
             }
@@ -56,7 +55,10 @@ class ArrayResolver implements DefinitionResolver
         return true;
     }
 
-    private function resolveDefinition(Definition $value, ArrayDefinition $definition, $key)
+    /**
+     * @throws DependencyException
+     */
+    private function resolveDefinition(Definition $value, ArrayDefinition $definition, int|string $key) : mixed
     {
         try {
             return $this->definitionResolver->resolve($value);
