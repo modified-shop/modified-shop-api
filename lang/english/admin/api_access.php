30f9e870e7e9b43bb5d40fc9e5e742bef7f98b76
Gerhard Waldemair <gerhard@modified-shop.org>
2026-06-25 23:55:25 +0200

    format file and add new header

--- lang/english/admin/api_access.php
+++ lang/english/admin/api_access.php
@@ -1,28 +1,30 @@
 <?php
-/* -----------------------------------------------------------------------------------------
-   $Id$
 
-   modified eCommerce Shopsoftware
-   http://www.modified-shop.org
+/**
+ * /lang/english/admin/api_access.php
+ *
+ * @package   modified-shop
+ * @link      https://www.modified-shop.org
+ *
+ * Copyright (c) modified eCommerce Shopsoftware
+ *
+ * Released under the GNU General Public License (GPL)
+ * https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-   Copyright (c) 2009 - 2021 [www.modified-shop.org]
-   -----------------------------------------------------------------------------------------
-   Released under the GNU General Public License
-   ---------------------------------------------------------------------------------------*/
-
-  define('TEXT_HEADING_CUSTOMERS', 'Customers');
-  define('TEXT_HEADING_CATEGORIES', 'Categories');
-  define('TEXT_HEADING_PRODUCTS', 'Products');
-  define('TEXT_HEADING_MANUFACTURERS', 'Manufacturers');
-  define('TEXT_HEADING_ATTRIBUTES', 'Attributes');
-  define('TEXT_HEADING_ORDERS', 'Orders');
-  define('TEXT_HEADING_COUNTRIES', 'Countries');
-  define('TEXT_HEADING_SHIPPING', 'Shipping');
-  define('TEXT_HEADING_CAMPAIGNS', 'Campaigns');
-  define('TEXT_HEADING_CURRENCIES', 'Currencies');
-  define('TEXT_HEADING_LANGUAGES', 'Languages');
-  define('TEXT_HEADING_NEWSLETTER', 'Newsletter');
-  define('TEXT_HEADING_CONFIGURATIONS', 'Configurations');
-  define('TEXT_HEADING_CONTENTS', 'Contents');
-  define('TEXT_HEADING_COUPONS', 'Coupons');
-  define('TEXT_HEADING_SCHEMA', 'Database Schema');
+define('TEXT_HEADING_CUSTOMERS', 'Customers');
+define('TEXT_HEADING_CATEGORIES', 'Categories');
+define('TEXT_HEADING_PRODUCTS', 'Products');
+define('TEXT_HEADING_MANUFACTURERS', 'Manufacturers');
+define('TEXT_HEADING_ATTRIBUTES', 'Attributes');
+define('TEXT_HEADING_ORDERS', 'Orders');
+define('TEXT_HEADING_COUNTRIES', 'Countries');
+define('TEXT_HEADING_SHIPPING', 'Shipping');
+define('TEXT_HEADING_CAMPAIGNS', 'Campaigns');
+define('TEXT_HEADING_CURRENCIES', 'Currencies');
+define('TEXT_HEADING_LANGUAGES', 'Languages');
+define('TEXT_HEADING_NEWSLETTER', 'Newsletter');
+define('TEXT_HEADING_CONFIGURATIONS', 'Configurations');
+define('TEXT_HEADING_CONTENTS', 'Contents');
+define('TEXT_HEADING_COUPONS', 'Coupons');
+define('TEXT_HEADING_SCHEMA', 'Database Schema');
